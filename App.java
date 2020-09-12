d74892e127a02de027b3e4421bf70c20a4144fab
danilorlovv <55670617+danilorlovv@users.noreply.github.com>
2020-09-12 12:55:43 +0300

    Add files via upload

--- App.java
+++ App.java
@@ -4,6 +4,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        String morda = "|0_0|";
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter text: ");
         String startText = scan.nextLine();
@@ -13,15 +14,27 @@ public class App
         {
             if (Character.isLetter(startText.charAt(i)))
             {
-                if (Character.isLowerCase(startText.charAt(i)))
-                    newText += Character.toUpperCase(startText.charAt(i));
+                if ((isVowel(startText.charAt(i))))
+                    newText += morda;
                 else
-                    newText += Character.toLowerCase(startText.charAt(i));
+                    newText += startText.charAt(i);
             }
         }
 
 
 
         System.out.println("We got you this: " + newText);
+    }
+    public static char[] vowels = new char[]{'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'Y', 'y'};
+    public static boolean isVowel(char c)
+    {
+        c = Character.toLowerCase(c);  //приводим символ в нижний регистр - от заглавных к строчным буквам
+
+        for (char d : vowels)   //ищем среди массива гласных
+        {
+            if (c == d)
+                return true;
+        }
+        return false;
     }
 }
